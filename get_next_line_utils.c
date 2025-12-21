f11be271d31701674815c789c39dabd6a9df1b60
elgheribikhira-star <elgheribikhira@gmail.com>
2025-12-21 13:14:51 +0400

    Refactor string manipulation functions in utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: kel-gher <kel-gher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 15:48:00 by kel-gher          #+#    #+#             */
-/*   Updated: 2025/12/19 17:54:11 by kel-gher         ###   ########.fr       */
+/*   Updated: 2025/12/21 10:05:32 by kel-gher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,81 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)&s[i]);
 	return (NULL);
 }
-static char	*join_when_s1_null(char const *s2)
+
+char *ft_strjoin_null(char *s1, const char *s2)
+{
+    size_t  len1;
+    size_t  len2;
+    char    *out;
+
+    if (!s2)
+        return (NULL);
+    if (!s1)
+        return (ft_strdup(s2));
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    out = malloc(len1 + len2 + 1);
+    if (!out)
+    {
+        free(s1);
+        return NULL;
+    }
+    ft_memcpy(out, s1, len1);
+    ft_memcpy(out + len1, s2, len2 + 1);
+    free(s1);
+    return out;
+}
+char	*ft_strdup(const char *s1)
 {
 	char	*res;
+	size_t	slen;
 	size_t	i;
 
-	if (!s2)
-		return (NULL);
-	res = malloc(ft_strlen(s2) + 1);
+	slen = ft_strlen(s1);
+	res = malloc(slen + 1);
 	if (!res)
 		return (NULL);
 	i = 0;
-	while (s2[i] != '\0')
+	while (i < slen)
 	{
-		res[i] = s2[i];
+		res[i] = s1[i];
 		i++;
 	}
 	res[i] = '\0';
 	return (res);
 }
+
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	const unsigned char	*s;
+	unsigned char		*d;
+
+	if (!dst && !src)
+		return (NULL);
+	d = dst;
+	s = src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
+}
+
 
 char	*ft_strjoin_null(char const *s1, char const *s2)
 {
