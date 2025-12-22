8cce3ef960c39739396afa23f3e2dceeb03cf46c
elgheribikhira-star <elgheribikhira@gmail.com>
2025-12-22 11:01:01 +0400

    Update BUFFER_SIZE and add new function prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -13,17 +13,19 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*ft_strchr(const char *s, int c);
-char *ft_strjoin_null(char *s1, const char *s2);
+char	*ft_strjoin_null(char *s1, const char *s2);
 size_t	ft_strlen(const char *str);
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*free_null(char **str, char *buff);
+char	*finish_line(char **str, char *buff);
 
 #endif
